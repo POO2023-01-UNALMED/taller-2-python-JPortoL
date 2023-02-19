c24e48c1255c2bfd782cf2612ca689a39265b80b
JPortoL <jporto@unal.edu.co>
2023-02-19 18:07:22 -0500

    intent one
    Fe y bless

---
+++ main.py
@@ -0,0 +1,54 @@
+class Asiento:
+    def __init__(self, color,precio,registro):
+        self.color=color
+        self.precio=precio
+        self.registro=registro
+    def cambiarColor(self,color):
+        if color == "rojo" or color == "verde" or color == "amarillo" or color == "negro" or color == "blanco":
+            self.color=color
+
+class Motor:
+    def __init__(self, numeroCilindros, tipo, registro):
+        self.numeroCilindros = numeroCilindros
+        self.tipo = tipo
+        self.registro = registro
+    def cambiarRegistro(self,registro):
+        self.registro = registro
+    def asignarTipo(self, tipo):
+        if tipo=="electrico" or tipo=="gasolina":
+            self.tipo = tipo
+
+class Auto:
+    def __init__(self, modelo, precio, asientos, marca, motor, registro, cantidadCreados):
+        self.modelo = modelo
+        self.precio = precio
+        self.asientos = asientos
+        self.marca = marca
+        self.motor = motor
+        self.registro = registro
+        cantidadCreados = cantidadCreados
+    def cantidadAsientos(self):
+        sum=0
+        for asiento in self.asientos:
+            if asiento != None:
+                sum+=1
+        return sum
+    def verificarIntegridad(self):
+        sum=0
+        if self.registro==self.motor.registro:
+            for asiento in self.asientos:
+                if asiento != None and asiento.registro != self.registro:
+                    sum+=1
+        else:
+            sum+=1
+        if sum>0:
+            return "Las piezas no son originales"
+        else:
+            return "Auto original"
+
+
+
+
+
+
+
